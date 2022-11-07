7d3f2d70ca62e9934e16223f9e3a504710afcd09
yamashita-a420 <ayanonagai420@gmail.com>
2022-11-07 14:45:23 +0900

    Update: 記事モデルにバリデーションと論理削除の読込設定を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ApplicationRecord
+  include Discard::Model
+
   belongs_to :employee
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :author, presence: true
 end
