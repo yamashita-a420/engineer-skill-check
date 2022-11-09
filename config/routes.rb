e4af0da9cf30c360c302864aa37c511e24959227
yamashita-a420 <ayanonagai420@gmail.com>
2022-11-09 17:09:41 +0900

    Update: ルーティングを３点変更・削除
    ルートパスをダッシュボードへ変更、社員showルーティングを削除、プロフィールと社員は1対1の関係のため、不要ルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  resources :dashboard, only: :index
   resources :articles, only: %i(index new create edit update destroy)
-  root 'employees#index'
+  root 'dashboard#index'
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  resources :employees do
-    resources :profiles
+  resources :employees, only: %i(index new create edit update destroy) do
+    resource :profile, only: %i(show edit update)
   end
 end
