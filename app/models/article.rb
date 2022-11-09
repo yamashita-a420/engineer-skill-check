f05f7cb62f69f433ea599f49070d235317a46748
yamashita-a420 <ayanonagai420@gmail.com>
2022-11-09 16:36:39 +0900

    Update: 論理削除されていない記事のスコープを作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,8 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :author, presence: true
+
+  scope :active, -> {
+    where(deleted_at: nil)
+  }
 end
