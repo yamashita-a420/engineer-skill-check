f10990db9cd1319baf54fde6e6782ab5fb983910
yamashita-a420 <ayanonagai420@gmail.com>
2022-11-26 21:05:56 +0900

    Update: ブートストラップ適用するため、フラッシュメッセージの種類を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   before_action :user_logged_in?
-  
+
+  add_flash_types :success, :info, :warning, :danger
+
   private
     def user_logged_in?
       return if logged_in?
